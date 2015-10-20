5354b1c0b44683070b90b4149f7d07022300ce4a
Dennis Wu <dennissfwu@gmail.com>
2015-10-20 23:11:34 +0800

    Support passing in options as child elements.

--- lib.jsx
+++ lib.jsx
@@ -1,8 +1,20 @@
 TAP = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData() {
+    var options;
+    if (this.props.children) {
+      options = this.props.children.reduce(function(map, child) {
+        if(child.type == 'option') {
+          map[child.key] = child.props.children;
+        }
+        return map;
+      }, {});
+    }
+    if (!options || Object.keys(options).length == 0) {
+      options = this.props.options;
+    }
     return {
-      text: TAPi18n.__(this.props.label, this.props.options)
+      text: TAPi18n.__(this.props.label, options)
     }
   },
   render() {
